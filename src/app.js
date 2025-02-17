105ce5bbd6f9c1c9e3b61fc8f1365f466fc0001b
ValenSilveira <silveirattini@gmail.com>
2025-02-17 20:55:55 +0000

    Agregar script para generar combinaciones de nombres de dominio

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our', 'your', 'my'];
+let adj = ['great', 'big', 'small', 'amazing'];
+let noun = ['jogger', 'racoon', 'developer', 'artist'];
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let p of pronoun) {
+  for (let a of adj) {
+    for (let n of noun) {
+      console.log(`${p}${a}${n}.com`);
+      console.log(`${p}${a}${n}.net`);
+      console.log(`${p}${a}${n}.us`);
+      console.log(`${p}${a}${n}.io`);
+    }
+  }
+}
+     
